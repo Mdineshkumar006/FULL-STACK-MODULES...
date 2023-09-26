0ff444e6b059e2c1eb8ab51c6c43433572d08f88
Mdineshkumar006 <145427645+Mdineshkumar006@users.noreply.github.com>
2023-09-26 13:41:53 +0530

    Add files via upload

---
+++ MODULE 2/gettingstandardwithjs.js
@@ -0,0 +1,25 @@
+// USUAL PRINT
+console.log("Getting started with JS");
+console.warm("Hello,How are you?");
+console.error("ERRRRORRR");
+
+// 1.) programming
+// A.) VARIABLES
+// initialization
+var age;// declaration
+var bag = "books";
+var fullname = "dineshkumar";
+bag = "Tiffin box";
+var count = 20 + 20;
+var englishMarks = 100;
+var hindiMarks = 98;
+console.log(englishMarks + hindiMarks);
+console.log(count);
+console.log(bag);
+age = 20;
+console.log(age);
+console.log(fullname);
+// 2.) Handling your views
+// ---> for controlling your html
+// ---> controlling your forms,para,heading,buttons
+// ---> DOM => Document object modeling
---
+++ MODULE 2/number.js
@@ -0,0 +1,27 @@
+// tostring()
+// toExponential()
+// toFixed()
+// toprecision()
+// valueof()
+
+var x = 120;
+//console.log(x);
+//console.log(x.toString());
+//console.log((120+80).tostring());
+
+var z = 9.76599999;
+
+//console.log(z);
+//console.log(z.toExponential(s));
+
+//console.log(z.toFixed(z));
+
+//console.log(z.toPrecision(4));
+//console.log((20 * 12).valueof());
+
+var age = '44';
+
+console.log(Number(age));
+console.log(Number(new Date("2020-01-02")));
+console.log(parseInt("837828"));
+console.log(parsefloat("23472843.233242"));
