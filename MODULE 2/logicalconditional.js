c9cf780e8fd389ba839d8884260a45a2916eabbd
Mdineshkumar006 <145427645+Mdineshkumar006@users.noreply.github.com>
2023-09-26 16:15:59 +0530

    Add files via upload

---
+++ MODULE 2/logicalconditional.js
@@ -0,0 +1,54 @@
+//var salary = 10000;
+//var employee = "Dine";
+
+//if(employee === "Dine" || salary >5000){
+  //  console.log("Increment");
+//}else{
+  //  console.log("Decrement");
+//}
+
+// ==
+// ===
+var name1 = "15";
+var name2 = 15;
+
+if(name1 === name2){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if(name1 === name2){
+    console.log("true");
+} else {
+    console.log("false");
+}
+// &&
+// ||
+
+//var age = 24;
+//var fname = 'Jee';
+
+
+//if (age > 18 || fname === 'Jee' ) {
+  //  console.log("Hello");
+//}else{
+  //  console.log("Bye");
+//}
+
+var salary = 5000;
+var hikecheck = salary > 2000
+? console.log("salary is fine")
+: console.log("salary Increment");
+
+console.log(hikeCheck);
+
+var contact = 9629448984;
+
+var contactcheck = contact === 9629448984 ? alert('true'): alert('false');
+
+//if(salary > 2000){
+  //  console.log("salary is fine");
+//}else{
+  //  console.log("salary Increment");
+//}
